5bb3e662778310e0e8a30d45282c6a88bc7b873e
Miracle- <abed90eg@gmail.com>
2026-06-23 00:26:47 +0300

    Main-Test

---
+++ warehouse-service/src/test/java/sverdlov/dev/warehouse_service/WarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sverdlov.dev.warehouse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
